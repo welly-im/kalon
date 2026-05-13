22e6c67b83df0e5113a3e212d8fc661afc56103d
welly-im <wellyimanuel86@gmail.com>
2026-05-13 08:54:49 +0700

    fix error ipo

--- src/app/ipoAraArbCalculator.js
+++ src/app/ipoAraArbCalculator.js
@@ -1,4 +1,4 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function IpoAraArbCalculator() {
